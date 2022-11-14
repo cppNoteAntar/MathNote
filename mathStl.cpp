e6c46ba23579f63715628cb492b4b189503df108
cppNoteAntar <118151423+cppNoteAntar@users.noreply.github.com>
2022-11-14 10:33:57 +0600

    Update mathStl.cpp

--- mathStl.cpp
+++ mathStl.cpp
@@ -5,20 +5,20 @@ int main()
 {         
 
 
-    // cout<<abs(-34);                     // ABSOLUTE 
-    // cout<<sqrt(49);                     // SQUARE 
-    // cout<<pow(6,2);                     // POWER
-    // cout<<log10(100);                   // LOG
+    // cout<<abs(-34);                     // ABSOLUTE output=34
+    // cout<<sqrt(49);                     // SQUARE output=7
+    // cout<<pow(6,2);                     // POWER output=36
+    // cout<<log10(100);                   // LOG   output=2
     // cout<<sin(45);                      // SIN
 
 
 
 
 
-    // cout<<round(5.5);                // ROUND if a>=0.5 it goes to next
-    // cout<<trunc(5.6);                // TRUNC
-    // cout<<ceil(5.1);                 // CEIL - PRINT THE UPPER VALUE
-    cout<<floor(5.2);                   // FLOOR - PRINT THE LOWER VALUE
+    // cout<<round(5.5);                // ROUND if a>=0.5 it goes to next  output=6
+    // cout<<trunc(5.6);                // TRUNC    output=5
+    // cout<<ceil(5.1);                 // CEIL - PRINT THE UPPER VALUE   output=6
+    cout<<floor(5.2);                   // FLOOR - PRINT THE LOWER VALUE    output=5 it same as trunc
  
     return 0;
 }

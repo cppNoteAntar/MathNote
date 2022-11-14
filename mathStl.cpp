e88e4f88feea4760b7e7341b2490235206085fde
cppNoteAntar <118151423+cppNoteAntar@users.noreply.github.com>
2022-11-14 10:32:04 +0600

    Add files via upload

---
+++ mathStl.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{         
+
+
+    // cout<<abs(-34);                     // ABSOLUTE 
+    // cout<<sqrt(49);                     // SQUARE 
+    // cout<<pow(6,2);                     // POWER
+    // cout<<log10(100);                   // LOG
+    // cout<<sin(45);                      // SIN
+
+
+
+
+
+    // cout<<round(5.5);                // ROUND if a>=0.5 it goes to next
+    // cout<<trunc(5.6);                // TRUNC
+    // cout<<ceil(5.1);                 // CEIL - PRINT THE UPPER VALUE
+    cout<<floor(5.2);                   // FLOOR - PRINT THE LOWER VALUE
+ 
+    return 0;
+}
